c543500863737e756f148a66d5a6bee4bb63a4b7
Matt Brzowski <5thhand@gmail.com>
2018-03-15 20:39:40 -0600

    add comments and change input box styling

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function executeTheseFunctions(e) {
   // highlightInputFields
   chrome.tabs.executeScript(null,
     {
-      code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].style.border = 'cadetblue 4px solid'}"
+      code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].style.border = '#bd5d38 4px solid'; inputs[i].style.borderRadius = '5px'}"
     });
   chrome.tabs.executeScript(null,
     {
