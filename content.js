e60a6d97d5eaeefbe6ba39221ca8e74f45dab0da
Matt Brzowski <5thhand@gmail.com>
2018-03-24 09:48:53 -0600

    refactor regex tests and move border coloring to content js

--- content.js
+++ content.js
@@ -1,209 +1,225 @@
 console.log("This content script is working");
 
 function fillInForm() {
-  const fNameRegex = /f(irst)?((\w+)*|\s|\w+\s|(\w+)?\W)name/i;
-  const lNameRegex = /l(ast)?((\w+)*|\s|\w+\s|(\w+)?\W)name/i;
-  const emailRegex = /e(\W)?mail/i;
-  const phoneRegex = /phone/i;
-  const addressRegex = /address/i;
-  const streetRegex = /street/i;
-  const cityRegex = /city/i;
-  const stateRegex = /state/i;
-  const zipRegex = /zip/i;
-  const companyNameRegex;
-  const companyAddressRegex;
-  const companyPhoneRegex;
-  const companyPositionRegex;
-  const schoolNameRegex;
-  const schoolAddressRegex;
-  const startYearRegex;
-  const endYearRegex;
-  const fieldOfStudyRegex;
-  const skillRegex;
-  const certificateRegex;
+  var fNameRegex = /f(irst)?((\w+)*|\s|\w+\s|(\w+)?\W)name/i;
+  var lNameRegex = /l(ast)?((\w+)*|\s|\w+\s|(\w+)?\W)name/i;
+  var emailRegex = /e(\W)?mail/i;
+  var phoneRegex = /phone/i;
+  var addressRegex = /address/i;
+  var streetRegex = /street/i;
+  var cityRegex = /city/i;
+  var stateRegex = /state/i;
+  var zipRegex = /zip/i;
+  var employerRegex = /employer/i;
+  var companyNameRegex = /PLACEHOLDERXX/;
+  var companyAddressRegex = /PLACEHOLDERXX/;
+  var companyPhoneRegex = /PLACEHOLDERXX/;
+  var companyPositionRegex = /PLACEHOLDERXX/;
+  var schoolNameRegex = /PLACEHOLDERXX/;
+  var schoolAddressRegex = /PLACEHOLDERXX/;
+  var startYearRegex = /PLACEHOLDERXX/;
+  var endYearRegex = /PLACEHOLDERXX/;
+  var fieldOfStudyRegex = /PLACEHOLDERXX/;
+  var skillRegex = /PLACEHOLDERXX/;
+  var certificateRegex = /PLACEHOLDERXX/;
 
-  let fName = localStorage.getItem('fname');
-  let lName = localStorage.getItem('lname');
-  let email = localStorage.getItem('email');
-  let phoneNumber = localStorage.getItem('phonenumber');
-  let streetAddress = localStorage.getItem('streetAddress');
-  let city = localStorage.getItem('city');
-  let state = localStorage.getItem('state');
-  let zipCode = localStorage.getItem('zipcode');
-  let companyName = localStorage.getItem('companyname');
-  let companyAddress = localStorage.getItem('companyaddress');
-  let companyPhone = localStorage.getItem('companyphone');
-  let companyPosition = localStorage.getItem('companyposition');
-  let schoolName = localStorage.getItem('schoolname');
-  let schoolAddress = localStorage.getItem('schooladdress');
-  let startYear = localStorage.getItem('startyear');
-  let endYear = localStorage.getItem('endyear');
-  let fieldOfStudy = localStorage.getItem('studyfield');
-  let skill = localStorage.getItem('skill');
-  let certificate = localStorage.getItem('certificate');
+  var fName = localStorage.getItem('fname') || "Matt";
+  var lName = localStorage.getItem('lname') || "Mattson";
+  var email = localStorage.getItem('email') || "mmattson@gmail.com";
+  var phoneNumber = localStorage.getItem('phonenumber') || "2012012011";
+  var streetAddress = localStorage.getItem('streetAddress') || "200 S 200 E Apt 200";
+  var city = localStorage.getItem('city') || "Salt Lake";
+  var state = localStorage.getItem('state') || "UT";
+  var zipCode = localStorage.getItem('zipcode') || "84567";
+  var companyName = localStorage.getItem('companyname') || "EZApply";
+  var companyAddress = localStorage.getItem('companyaddress') || "101 Main St.";
+  var companyPhone = localStorage.getItem('companyphone') || "2012012011";
+  var companyPosition = localStorage.getItem('companyposition') || "CEO";
+  var schoolName = localStorage.getItem('schoolname') || "UofU";
+  var schoolAddress = localStorage.getItem('schooladdress') || "101 University Blvd.";
+  var startYear = localStorage.getItem('startyear') || "2001";
+  var endYear = localStorage.getItem('endyear') || "1999";
+  var fieldOfStudy = localStorage.getItem('studyfield') || "Software Dev";
+  var skill = localStorage.getItem('skill') || "Gamez";
 
-  let $inputs = $("input");
-  let $labels = $("label");
+  var $inputs = $("input");
+  var $labels = $("label");
   console.log($inputs);
   console.log($labels);
 
+  function fillInput(inputObj, value) {
+    $(inputObj).css('border', '#bd5d38 4px solid');
+    $(inputObj).css('borderRadius', '5px');
+    $(inputObj).val(value);
+  }
+
   // loop through input elements
   for (elt in $inputs) {
+    debugger;
 
-    // ==============================
-    // First we use Labels to test against our Regular Expressions as not every page has labels
-    // ==============================
-    for (label in $labels) {
+    if ($inputs[elt].type !== 'hidden' && $inputs[elt].type !== 'radio') {
+      // ==============================
+      // First we use Labels to test against our Regular Expressions as not every page has labels
+      // ==============================
+      for (label in $labels) {
 
-      // Ensures the current label element has a for attribute
-      if ($labels[label].attributes && $labels[label].attributes["for"]) {
+        // Ensures the current label element has a for attribute
+        if ($labels[label].attributes && $labels[label].attributes["for"]) {
 
-        // If the for attribute matches the input element's id, check regex
-        if ($labels[label].attributes["for"].nodeValue === $inputs[elt].id || $labels[label].attributes["for"].nodeValue === $inputs[elt].name) {
+          // If the for attribute matches the input element's id, check regex
+          if ($labels[label].attributes["for"].nodeValue === $inputs[elt].id || $labels[label].attributes["for"].nodeValue === $inputs[elt].name) {
 
-          // ===========================
-          // Use Regex to test labels and autofill
-          // ===========================
-          const labelText = $labels[label].innerText;
-          const input = $($inputs[elt]);
+            // ===========================
+            // Use Regex to test labels and autofill
+            // ===========================
+            var labelText = $labels[label].innerText;
+            var input = $($inputs[elt]);
 
-          if (fNameRegex.test(labelText)) {
-            input.val(fName);
-          } else if (lNameRegex.test(labelText)) {
-            input.val(lName);
-          } else if (emailRegex.test(labelText)) {
-            input.val(email)
-          } else if (phoneRegex.test(labelText)) {
-            input.val(phoneNumber);
-          } else if (addressRegex.test(labelText) || streetRegex.test(labelText)) {
-            input.val(streetAddress);
-          } else if (cityRegex.test(labelText)) {
-            input.val(city);
-          } else if (stateRegex.test(labelText)) {
-            input.val(state);
-          } else if (companyNameRegex.test(labelText)) {
-            input.val(companyName);
-          } else if (companyAddressRegex.test(labelText)) {
-            input.val(companyAddress);
-          } else if (companyPhoneRegex.test(labelText)) {
-            input.val(companyPhone);
-          } else if (companyPositionRegex.test(labelText)) {
-            input.val(companyPosition);
-          } else if (schoolNameRegex.test(labelText)) {
-            input.val(schoolName);
-          } else if (schoolAddressRegex.test(labelText)) {
-            input.val(schoolAddress);
-          } else if (startYearRegex.test(labelText)) {
-            input.val(startYear);
-          } else if (endYearRegex.test(labelText)) {
-            input.val(endYear);
-          } else if (companyAddressRegex.test(labelText)) {
-            input.val(companyAddress);
-          } else if (fieldOfStudyRegex.test(labelText)) {
-            input.val(fieldOfStudy);
-          } else if (skillRegex.test(labelText)) {
-            input.val(skill);
-          } else if (certificateRegex.test(labelText)) {
-            input.val(certificate);
+            if (fNameRegex.test(labelText)) {
+              fillInput(input, fName);
+            } else if (lNameRegex.test(labelText)) {
+              fillInput(input, lName);
+            } else if (emailRegex.test(labelText)) {
+              fillInput(input, email);
+            } else if (phoneRegex.test(labelText)) {
+              fillInput(input, phoneNumber);
+            } else if (addressRegex.test(labelText) || streetRegex.test(labelText)) {
+              fillInput(input, streetAddress);
+            } else if (cityRegex.test(labelText)) {
+              fillInput(input, city);
+            } else if (stateRegex.test(labelText)) {
+              fillInput(input, state);
+            } else if (zipRegex.test(labelText)) {
+              fillInput(input, zipCode);
+            } else if (companyNameRegex.test(labelText)) {
+              fillInput(input, companyName);
+            } else if (companyAddressRegex.test(labelText)) {
+              fillInput(input, companyAddress);
+            } else if (companyPhoneRegex.test(labelText)) {
+              fillInput(input, companyPhone);
+            } else if (companyPositionRegex.test(labelText)) {
+              fillInput(input, companyPosition);
+            } else if (schoolNameRegex.test(labelText)) {
+              fillInput(input, schoolName);
+            } else if (schoolAddressRegex.test(labelText)) {
+              fillInput(input, schoolAddress);
+            } else if (startYearRegex.test(labelText)) {
+              fillInput(input, startYear);
+            } else if (endYearRegex.test(labelText)) {
+              fillInput(input, endYear);
+            } else if (companyAddressRegex.test(labelText)) {
+              fillInput(input, companyAddress);
+            } else if (fieldOfStudyRegex.test(labelText)) {
+              fillInput(input, fieldOfStudy);
+            } else if (skillRegex.test(labelText)) {
+              fillInput(input, skill);
+            } 
           }
         }
+
       }
     }
+
+
 
 
     // ===============================
     // After we look for labels, we use input elements to look for identifying info
     // ===============================
 
-
     // First name
     if (fNameRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(fName);
+      fillInput($inputs[elt], fName);
     }
 
     // Last name
     if (lNameRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(lName);
+      fillInput($inputs[elt], lName);
     }
 
     // Email
     if (emailRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(email);
+      fillInput($inputs[elt], email);
+    }
+
+    // Phone
+    if (phoneRegex.test($inputs[elt].outerHTML)) {
+      fillInput($inputs[elt], phoneNumber);
     }
 
     // Address
     if (addressRegex.test($inputs[elt].outerHTML) || streetRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(streetAddress);
+      fillInput($inputs[elt], streetAddress);
     }
 
     // City
     if (cityRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(city);
+      fillInput($inputs[elt], city);
     }
 
     // State
     if (stateRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(state);
+      fillInput($inputs[elt], state);
     }
 
     // Zipcode
     if (zipRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(zipCode);
+      fillInput($inputs[elt], zipCode);
     }
 
     // Company Name
     if (companyNameRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(companyName);
+      fillInput($inputs[elt], companyName);
     }
 
     // Company Address
     if (companyAddressRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(companyAddress);
+      fillInput($inputs[elt], companyAddress);
     }
 
     // Company Phone
     if (companyPhoneRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(companyPhone);
+      fillInput($inputs[elt], companyPhone);
     }
 
     // Company Position
-    if (companyPosition.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(companyPosition);
+    if (companyPositionRegex.test($inputs[elt].outerHTML)) {
+      fillInput($inputs[elt], companyPosition);
     }
 
     // School Name
     if (schoolNameRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(schoolName);
+      fillInput($inputs[elt], schoolName);
     }
 
     // School Address
     if (schoolAddressRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(schoolAddress);
+      fillInput($inputs[elt], schoolAddress);
     }
 
     // Start Year
     if (startYearRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(startYear);
+      fillInput($inputs[elt], startYear);
     }
 
     // End Year
     if (endYearRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(endYear);
+      fillInput($inputs[elt], endYear);
     }
 
     // Field of Study
     if (fieldOfStudyRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(fieldOfStudy);
+      fillInput($inputs[elt], fieldOfStudy);
     }
 
     // Skill 
     if (skillRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(skill);
+      fillInput($inputs[elt], skill);
     }
 
     // Certification
     if (certificateRegex.test($inputs[elt].outerHTML)) {
-      $($inputs[elt]).val(certificate);
+      fillInput($inputs[elt], certificate);
     }
   }
 }
--- popup.js
+++ popup.js
@@ -1,15 +1,10 @@
 let el = document.getElementById('work-button');
 if (el) {
-  document.getElementById('work-button').addEventListener('click', executeTheseFunctions);
+  document.getElementById('work-button').addEventListener('click', runAutofill);
 }
 
-function executeTheseFunctions(e) {
+function runAutofill(e) {
 
-  // highlightInputFields
-  chrome.tabs.executeScript(null,
-    {
-      code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].style.border = '#bd5d38 4px solid'; inputs[i].style.borderRadius = '5px'}"
-    });
   chrome.tabs.executeScript(null,
     {
       code: "fillInForm();"
@@ -17,3 +12,12 @@ function executeTheseFunctions(e) {
 
   window.close();
 }
+
+// chrome.tabs.executeScript(null,
+//   {
+//     code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].style.border = '#bd5d38 4px solid'; inputs[i].style.borderRadius = '5px'}"
+
+//     $($inputs[elt]).css('border', '#bd5d38 4px solid');
+//     $($inputs[elt]).css('borderRadius', '5px');
+    
+//   });
